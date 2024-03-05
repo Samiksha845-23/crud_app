821e35ff269d64ed2f2ac5d13adc1c2e95873868
Samiksha Khandelwal <samikshamau5678@gmail.com>
2024-03-05 14:44:39 +0530

    third coomit

--- src/Components/Read.jsx
+++ src/Components/Read.jsx
@@ -1,29 +1,24 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { showUser } from "../features/userDetailSlice";
+
 const Read = () => {
-  const dispatch = useDispatch();
-  const { users, loading } = useSelector((state) => state.app);
-  useEffect(() => {
-    dispatch(showUser());
-  }, []);
   return (
     <div>
       <h2>All Data </h2>
-      <div>
-        <div className="card w--50 mx-auto">
-          <img src="..." className="card-img-top" alt="..." />
+
+
+      <div><div div className = "card w--50 mx-auto" >
           <div className="card-body">
-            <h5 className="card-title">Card title</h5>
+            <h5 className="card-title">Create List</h5>
             <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              
             </p>
             <a href="#" className="btn btn-primary">
               Go somewhere
             </a>
           </div>
-        </div>
+          </div>
       </div>
     </div>
   );
--- src/features/userDetailSlice.js
+++ src/features/userDetailSlice.js
@@ -1,38 +1,46 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 //create action
-export const createUser = createAsyncThunk("createUser", async (data, { rejectWithValue }) => {
-  const response = await fetch(
-    "https://65bfecc225a83926ab95dc58.mockapi.io/crud",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
+export const createUser = createAsyncThunk(
+  "createUser",
+  async (data, { rejectWithValue }) => {
+    const response = await fetch(
+      "https://65bfecc225a83926ab95dc58.mockapi.io/crud",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }
+    );
+
+    try {
+      const result = await response.json();
+      return result;
+    } catch (error) {
+      return rejectWithValue(error);
     }
-  );
-
-  try {
-    const result = await response.json();
-    return result;
-  } catch (error) {
-    return rejectWithValue(error);
   }
-});
+);
 
-//read action 
-export const showUser = createAsyncThunk("showUser", async (rejectWithValue) => {
-  const response =await  fetch("https://65bfecc225a83926ab95dc58.mockapi.io/crud");
+//read action
+export const showUser = createAsyncThunk(
+  "showUser",
+  async ({ rejectWithValue }) => {
+    const response = await fetch(
+      "https://65bfecc225a83926ab95dc58.mockapi.io/crud"
+    );
 
-  try {
-    const result = await response.json();
-    console.log(result);
-    return result;
-  } catch (error) {
-    return rejectWithValue(error);
+    try {
+      const result = await response.json();
+      console.log(result);
+      return result;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
   }
-});
+);
 
 export const userDetail = createSlice({
   name: "userDetail",
@@ -41,29 +49,31 @@ export const userDetail = createSlice({
     loading: false,
     error: null,
   },
-  extraReducers: {
-    [createUser.pending]: (state) => {
-      state.loading = true;
-    },
-    [createUser.fulfilled]: (state, action) => {
-      state.loading = false;
-      state.users.push(action.payload);
-    },
-    [createUser.rejected]: (state, action) => {
-      state.loading = false;
-      state.users = action.payload;
-    },
-    [showUser.pending]: (state) => {
-      state.loading = true;
-    },
-    [showUser.fulfilled]: (state, action) => {
-      state.loading = false;
-      state.users = action.payload;
-    },
-    [showUser.rejected]: (state, action) => {
-      state.loading = false;
-      state.users = action.payload.message;
-    },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(createUser.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(createUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.users.push(action.payload);
+      })
+      .addCase(createUser.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload.message;
+      })
+      .addCase(showUser.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(showUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.users = action.payload;
+      })
+      .addCase(showUser.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload.message;
+      });
   },
 });
 
