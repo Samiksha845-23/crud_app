8e4c04619f209a6294793f74c3788c44a966bf86
Samiksha Khandelwal <samikshamau5678@gmail.com>
2024-03-05 15:58:10 +0530

    Revert "third commit"
    This reverts commit ebd8b464d5340bdaedd730c8165d3088284d8ba9.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import React from "react";
 import Navbar from "./Components/Navbar";
 import Create from "./Components/Create";
-import Read from "./Components/Read";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
         <Navbar />
         <Routes>
           <Route exact path="/" element={<Create />} />
-          <Route exact path="/read" element={<Read />} />
         </Routes>
       </BrowserRouter>
     </div>
